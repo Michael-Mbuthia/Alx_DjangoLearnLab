ee91209164d1ab6a67fd498bfc97ec48067b69a7
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2026-01-18 23:25:24 +0300

    Refactor import statements in views.py to separate permission_required decorator

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth import login
 from .models import Book
 from .models import Library
