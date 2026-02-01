10c33e20c8933766a3c3c945976bfd6462ae9ca7
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2026-02-01 08:36:13 +0300

    Add permissions to Book model for enhanced access control

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -47,3 +47,11 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField(default=2000)
+
+    class Meta:
+        permissions = [
+            ('can_view', 'Can view book'),
+            ('can_create', 'Can create book'),
+            ('can_edit', 'Can edit book'),
+            ('can_delete', 'Can delete book'),
+        ]
