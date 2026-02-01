d6e55577b6079568bd2a014eb0005d0e6c9ce5d6
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2026-02-01 11:09:11 +0300

    Add API token authentication route to URL configuration

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 
 from .views import BookList, BookViewSet
@@ -9,6 +10,8 @@ router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    path('api-token-auth/', obtain_auth_token, name='api-token-auth'),
+
     # Route for the BookList view (ListAPIView)
     path('books/', BookList.as_view(), name='book-list'),
 
