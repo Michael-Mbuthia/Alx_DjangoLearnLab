ca3a49d7343e5c575eaceacf7e928477ed1f851f
Michael-Mbuthia <michaelmbuthia50@gmail.com>
2026-02-15 20:02:46 +0300

    Add initial migration and Post model definition

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+	published_date = models.DateTimeField(auto_now_add=True)
+	author = models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		on_delete=models.CASCADE,
+		related_name='posts',
+	)
